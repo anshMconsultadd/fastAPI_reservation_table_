b55c32b77d2518953c28feaff3d37cc97466fc50
anshMconsultadd <ansh.m@consultadd.com>
2025-02-02 11:41:08 +0530

    added cors for frontend

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from app.auth import router as auth_router
 from app.routes.admin import router as admin_router
 from app.routes.user import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change "*" to frontend URL later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
